5868b22791572c4c6681959effad41d256b69956
Lonerr7 <krabik2342@gmail.com>
2023-10-25 19:49:17 +0300

    4. Completed route auth handlers

--- app/api/auth/[...nextAuth]/route.ts
+++ app/api/auth/[...nextAuth]/route.ts
@@ -1,6 +1,13 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
+import { connectToDB } from '@/utils/connectToDB';
+import User from '@/models/User';
+import { JWT } from 'next-auth/jwt';
+import { AdapterUser } from 'next-auth/adapters';
+
+// Остановился на 1:24:32
+
 console.log({
   clientId: process.env.GOOGLE_CLIENT_ID!,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
@@ -13,9 +20,47 @@ const handlder = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  async session({ session }: { session: any }) {},
+  callbacks: {
+    async session({
+      session,
+      token,
+      user,
+    }: {
+      session: any;
+      token: JWT;
+      user: AdapterUser;
+    }) {
+      const sessionUser = await User.findOne({
+        email: session.user?.email,
+      });
 
-  async signIn({ profile }: { profile: any }) {},
+      session.user.id = sessionUser._id.toString();
+
+      return session;
+    },
+
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+        // 1. Check if the user already exists
+        const user = await User.findOne({ email: profile?.email });
+
+        // 2. If user doesn't exist, create it
+        if (!user) {
+          await User.create({
+            email: profile?.email,
+            username: profile?.name?.replace(' ', '').toLowerCase(),
+            image: profile?.image,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
 });
 
 export { handlder as GET, handlder as POST };
